ab22a526ed35eeb4906185b67e89bd541a385bd1
Velho <jewel960@gmail.com>
2014-10-10 22:43:00 +0300

    Subtitle class skeleton added.

---
+++ src/Subtitle.h
@@ -0,0 +1,68 @@
+#ifndef SUBTITLE_H
+#define SUBTITLE_H
+
+#include <string>
+
+/*!
+ * \brief The Subtitle class
+ * Abstraction of the subtitle table.
+ * Style is not applied here.
+ *
+ * TODO This class will prop need refactoring when
+ * new formats come into play.
+ */
+class Subtitle {
+public:
+    /*!
+     * \brief Subtitle
+     * Constructs the subtitle class.
+     * \param idx
+     * Index of the subtitle.
+     * \param start
+     * Start frame for the subtitle.
+     * \param end
+     * End frame for the subtitle.
+     * \param t
+     * The actual subtitle text.
+     */
+    Subtitle(int idx, int start, int end, std::string t) :
+        index{ idx },startframe{ start }, endframe{ end }, text{ t }
+    { }
+
+    /*!
+     * \brief getIndex
+     * Index value(Unique, Primary).
+     * \return
+     * int index
+     */
+    int getIndex() const { return index; }
+    /*!
+     * \brief getStartFrame
+     * Start frame.
+     * \return
+     * int startframe
+     */
+    int getStartFrame() const { return startframe; }
+    /*!
+     * \brief getEndFrame
+     * End frame.
+     * \return
+     * int endframe
+     */
+    int getEndFrame() const { return endframe; }
+    /*!
+     * \brief getText
+     * Subtitle text.
+     * \return
+     * std::string text;
+     */
+    std::string getText() const { return text; }
+
+private:
+    int index;
+    int startframe;
+    int endframe;
+    std::string text;
+};
+
+#endif // SUBTITLE_H
